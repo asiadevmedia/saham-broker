45db79c85ab9d574916e0eab98afe4c5432dc34a
Teguh Priyatno <asiadevmedia@gmail.com>
2018-11-29 23:23:23 +0700

    FIX 6.20.1
    FIX 6.20.1 29-11-2018

--- saham_broker/func.py
+++ saham_broker/func.py
@@ -1,5 +1,5 @@
 # A token you get from the Telegram's botfather
-telegrambot = ''
+telegrambot = '740342159:AAHo4soiyucP9a33K3xVbGxcwndV680mPNI'
 
 # A chat_id of your client
 adminId = [446393882, -1001349564354]
